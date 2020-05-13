391c391b6900995b15102724f908725755b3babe
susu <lk19931990@163.com>
2020-05-13 21:01:30 +0800

    初始化

---
+++ src/main/java/GQJ/home/hegs/ServletInitializer.java
@@ -0,0 +1,13 @@
+package GQJ.home.hegs;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HegsApplication.class);
+	}
+
+}
---
+++ src/test/java/GQJ/home/hegs/HegsApplicationTests.java
@@ -0,0 +1,13 @@
+package GQJ.home.hegs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HegsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
