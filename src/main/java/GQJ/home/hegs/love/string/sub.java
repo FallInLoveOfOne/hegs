a40024addadbeb9673f243f7106efb133c4d3b32
susu <lk19931990@163.com>
2020-05-13 21:22:28 +0800

    添加方法

---
+++ src/main/java/GQJ/home/hegs/love/string/sub.java
@@ -0,0 +1,15 @@
+package GQJ.home.hegs.love.string;
+
+/**
+ * 字符串截取测试
+ */
+public class sub {
+
+    public static void main(String[] args) {
+        // 字符串初始角标为：0
+        String val = "0123456789";
+        System.out.println(val.substring(1));
+        // 从第二位开始截取，截取（8-1）位：左闭右开[1,8)
+        System.out.println(val.substring(1,8));
+    }
+}
