cf5dbd0772b27368ea6341be107f867b971efae1
suh <2220388107@qq.com>
2020-09-10 14:45:13 +0800

    添加mvc

--- src/main/java/GQJ/home/hegs/love/util/RanDom.java
+++ src/main/java/GQJ/home/hegs/love/util/RanDom.java
@@ -27,6 +27,7 @@ public class RanDom {
 
     }
 
+    static List<Long> sources = Stream.of(10L, 20L, 30L, 40L).collect(Collectors.toList());
     static Integer order = 1;
 
     /**
@@ -36,6 +37,32 @@ public class RanDom {
      * @return
      */
     public static Long rand(List<Long> sources) {
+        System.out.println("=========" + order + "=========");
+        if (null == sources || sources.isEmpty()) {
+            return 99999L;
+        }
+        Integer size = sources.size();
+        Random random = new Random();
+        Integer integer = random.nextInt(size);
+        Long result = sources.get(integer);
+        System.out.println("第" + order + "次中奖编码:" + result);
+        sources = sources.stream().filter(val -> {
+            if (!val.equals(result)) {
+                return true;
+            }
+            return false;
+        }).collect(Collectors.toList());
+        System.out.println("未抽中人员编码:");
+        sources.forEach(val -> {
+            System.out.println(val);
+        });
+        System.out.println("=========" + order + "=========");
+        order = (order + 1);
+        rand(sources);
+        return result;
+    }
+
+    public static Long rand() {
         System.out.println("=========" + order + "=========");
         if (null == sources || sources.isEmpty()) {
             return 99999L;
