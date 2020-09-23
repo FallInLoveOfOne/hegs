fefa66cf702135d1d770930e136bad96c1d6dd62
suh <2220388107@qq.com>
2020-09-23 14:49:42 +0800

    解释接口的向后兼容

--- src/main/java/GQJ/home/hegs/love/lambda/Child.java
+++ src/main/java/GQJ/home/hegs/love/lambda/Child.java
@@ -18,7 +18,13 @@ public class Child extends Parent implements IChild, ICh {
 
     @Override
     public void say() {
-        // 类胜于接口，目的向后兼容
+        /**
+         * 类胜于接口，目的向后兼容
+         * 假设已实现了一个定制的列表 MyCustomList ，该类中有一个 addAll 方法，如果新的 List
+         * 类库 ｜ 45
+         * 接口也增加了一个默认方法 addAll ，该方法将对列表的操作代理到 add 方法。如果类中重
+         * 写的方法没有默认方法的优先级高，那么就会破坏已有的实现。
+         */
         super.say();
     }
 
