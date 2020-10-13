15551a2845bbb024683064a15437a1d917de1da0
suh <2220388107@qq.com>
2020-10-13 17:36:19 +0800

    ++i与i++原理解析

--- src/main/java/GQJ/home/hegs/love/classLoad/ClasLoadMain.java
+++ src/main/java/GQJ/home/hegs/love/classLoad/ClasLoadMain.java
@@ -18,5 +18,18 @@ public class ClasLoadMain {
         Arrays.asList(urLs).forEach(val -> {
             System.out.println(val.toExternalForm());
         });
+        // 栈帧中的局部变量表与操作栈
+        int i = 1;
+        // i先入操作栈，然后在局部变量上+1,栈顶未+1的值赋给a
+        int a = i++;
+        // 1
+        System.out.println("a:" + a);
+        System.out.println("i:" + i);
+        int n = 1;
+        // n先在局部变量表+1，然后入操作栈,栈顶+1的值赋给b
+        int b = ++n;
+        // 2
+        System.out.println("b:" + b);
+        System.out.println("n:" + n);
     }
 }
